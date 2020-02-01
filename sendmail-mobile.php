fd8133822eb0f8e4a0ba3de4b2ea9b62cdaa7aa9
chalatz <chralatz@gmail.com>
2020-02-01 19:35:54 +0200

    Added honeypot fields

--- sendmail-mobile.php
+++ sendmail-mobile.php
@@ -56,18 +56,16 @@ $msg .= "Zip Code: $zipCode\r\n";
 $msg .= "Time Zone: $timeZone\r\n";
 
 function passed(){
-    // if(isset($_POST['meli_tria'])){
-    //     $meli_tria_passed = false;
-    // } else {
-    //     $meli_tria_passed = true;
-    // }
-    // if ($_POST['meli_ena'] == '' && $_POST['meli_dio'] == '' && $meli_tria_passed){
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-
-    return true;
+    if(isset($_POST['meli_tria'])){
+        $meli_tria_passed = false;
+    } else {
+        $meli_tria_passed = true;
+    }
+    if ($_POST['meli_ena'] == '' && $_POST['meli_dio'] == '' && $meli_tria_passed){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 if(passed() && mail($address, $e_subject, $msg, "From: $e_mail\r\nReply-To: $e_mail\r\nReturn-Path: $e_mail\r\nContent-Type: text/plain; charset=UTF-8\r\n"))
